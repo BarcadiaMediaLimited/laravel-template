00ef5a5e9ea7b1a57136f43549b6d16dee1b40ad
Steve McDougall <juststevemcd@gmail.com>
2022-09-09 14:55:19 +0100

    Installing alpine and tailwindcss requirements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import _ from 'lodash';
+import Alpine from 'alpinejs';
+import axios from 'axios';
+
+window._ = _;
+window.axios = axios;
+window.Alpine = Alpine;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Alpine.start();
