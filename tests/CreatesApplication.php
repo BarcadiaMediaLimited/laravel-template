1b4535dba233443802fbb166c6460e61035aeea5
Steve McDougall <juststevemcd@gmail.com>
2022-09-09 14:40:41 +0100

    Installing and setting up Pest PHP

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
+    public function createApplication(): Application
     {
+        /**
+         * @var Application $app
+         */
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make(Kernel::class)->bootstrap();
+        $app->make(
+            abstract: Kernel::class,
+        )->bootstrap();
 
         return $app;
     }
